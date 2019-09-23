5877b18cff083de3955a65c654e22bd848cc2bc8
StanBorovko <34895740+StanBorovko@users.noreply.github.com>
2019-09-23 19:16:35 +0300

    Updates

---
+++ precache-manifest.b1a6c245040ab37cf045411f808a481a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bba53d6499cd6f0f6adaeadd7b42a6",
+    "url": "/CountriesDB/index.html"
+  },
+  {
+    "revision": "6afc22d8b81b016fd32f",
+    "url": "/CountriesDB/static/css/main.31092e05.chunk.css"
+  },
+  {
+    "revision": "4357a2a5291a9863a105",
+    "url": "/CountriesDB/static/js/2.1d981292.chunk.js"
+  },
+  {
+    "revision": "6afc22d8b81b016fd32f",
+    "url": "/CountriesDB/static/js/main.b78cecfd.chunk.js"
+  },
+  {
+    "revision": "8b5b5f3356b2ed2e938f",
+    "url": "/CountriesDB/static/js/runtime~main.0a3268c5.js"
+  },
+  {
+    "revision": "5a70571cb67e146c71030cef16db4f60",
+    "url": "/CountriesDB/static/media/error.5a70571c.svg"
+  },
+  {
+    "revision": "b50cddc535735c297ff949b78fefdbd5",
+    "url": "/CountriesDB/static/media/globe.b50cddc5.svg"
+  }
+]);
